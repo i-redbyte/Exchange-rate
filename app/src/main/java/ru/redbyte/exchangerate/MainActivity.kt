32292dcf876b102bdd6fba2f0856d3c3c82b2d72
Ilya Sokolov <developer.sokolov@gmail.com>
2019-11-04 20:18:34 +0300

    initial commit

---
+++ app/src/main/java/ru/redbyte/exchangerate/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.redbyte.exchangerate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
