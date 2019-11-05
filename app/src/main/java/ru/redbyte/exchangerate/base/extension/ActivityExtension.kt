8a0f03b7199a0c63646bf2d19e866933ad3aa11f
Ilya Sokolov <developer.sokolov@gmail.com>
2019-11-05 15:17:52 +0300

    add: activity extension and base delegation adapter

---
+++ app/src/main/java/ru/redbyte/exchangerate/base/extension/ActivityExtension.kt
@@ -0,0 +1,31 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package ru.redbyte.exchangerate.base.extension
+
+import android.app.Activity
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+fun Activity?.hideKeyboard(view: View? = this?.currentFocus) {
+    this ?: return
+    view ?: return
+    val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+inline fun AppCompatActivity.setActionBar(
+        toolbar: Toolbar?,
+        crossinline block: ActionBar.() -> Unit
+) {
+    setSupportActionBar(toolbar)
+    supportActionBar?.block()
+}
--- app/src/main/java/ru/redbyte/exchangerate/presentation/main/CurrencyExchangeActivity.kt
+++ app/src/main/java/ru/redbyte/exchangerate/presentation/main/CurrencyExchangeActivity.kt
@@ -2,19 +2,37 @@ package ru.redbyte.exchangerate.presentation.main
 
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_currency_exchange.*
 import ru.redbyte.exchangerate.R
 import ru.redbyte.exchangerate.base.BaseActivity
+import ru.redbyte.exchangerate.base.extension.setActionBar
 
 class CurrencyExchangeActivity : BaseActivity<CurrencyExchangeContract.Presenter>(),
-        CurrencyExchangeContract.View {
+    CurrencyExchangeContract.View {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_currency_exchange)
+        setupView()
         presenter.start()
+    }
+
+    private fun setupView() {
+        setupActionBar()
+        setupRecyclerView()
+        btnExchange.setOnClickListener { }
+    }
+
+    private fun setupRecyclerView() {
+
     }
 
     override fun showError(message: String?) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun setupActionBar() = setActionBar(findViewById(R.id.tActionBar)) {
+        setTitle(R.string.app_name)
+        setDisplayHomeAsUpEnabled(false)
     }
 }
